d1c91e5a44a5185ca29ecebc6d2d93f044d06928
hondatoshihiro <harutokaho@gmail.com>
2024-04-30 03:31:18 +0900

    std:list, std::vector, std::set

--- study04/main01.cpp
+++ study04/main01.cpp
@@ -2,8 +2,11 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <set>
 
-
+//ビルド:>g++ main01.cpp
+//ビルド(最適化):>g++ -O2 main01.cpp
+//実行:>./a.out
 int main(){
     const long MAX_CNT = 100000;
 
@@ -15,7 +18,7 @@ int main(){
     //ミリ秒単位で計測
     std::chrono::duration<double, std::milli> elapsed_msec;
 
-#if 0  //std::list
+#if 1  //std::list
     start = std::chrono::system_clock::now();
     //要素登録
     std::list<long> oList;
@@ -41,8 +44,8 @@ int main(){
     //順次検索:iterator(2)登録
     lSum = 0;
     start = std::chrono::system_clock::now();
-    std::list<long>::iterator itE = oList.end();
-    for(std::list<long>::iterator it = oList.begin(); it!=itE; it++){
+    std::list<long>::iterator itListE = oList.end();
+    for(std::list<long>::iterator it = oList.begin(); it!=itListE; it++){
         lSum += (*it);
     }
     end = std::chrono::system_clock::now();
@@ -76,8 +79,8 @@ int main(){
     //順次検索:iterator(2)
     lSum = 0;
     start = std::chrono::system_clock::now();
-    std::vector<long>::iterator itE = oVec.end();
-    for(std::vector<long>::iterator it = oVec.begin(); it!=itE; it++){
+    std::vector<long>::iterator itVecE = oVec.end();
+    for(std::vector<long>::iterator it = oVec.begin(); it!=itVecE; it++){
         lSum += (*it);
     }
     end = std::chrono::system_clock::now();
@@ -85,26 +88,85 @@ int main(){
     //結果出力
     std::cout << "順次検索,iterator(2),std::vector," << elapsed_msec.count() << ",ミリ秒,lSum," << lSum << std::endl;
 
-    //順次検索:[]オペレータ
+    //順次検索:[]オペレータ(1)
     lSum = 0;
     start = std::chrono::system_clock::now();
-    for(long l = 0; l < MAX_CNT; l++){
+    for(long l = 0; l < oVec.size(); l++){
         lSum += oVec[l];
     }
     end = std::chrono::system_clock::now();
     elapsed_msec =  end - start;
     //結果出力
-    std::cout << "順次検索,[]オペレータ,std::vector," << elapsed_msec.count() << ",ミリ秒,lSum," << lSum << std::endl;
+    std::cout << "順次検索,[]オペレータ(1),std::vector," << elapsed_msec.count() << ",ミリ秒,lSum," << lSum << std::endl;
 
-    //順次検索:atメソッド登録
+    //順次検索:[]オペレータ(2)
     lSum = 0;
     start = std::chrono::system_clock::now();
-    for(long l = 0; l < MAX_CNT; l++){
+    long opMaxSize = oVec.size();
+    for(long l = 0; l < opMaxSize; l++){
+        lSum += oVec[l];
+    }
+    end = std::chrono::system_clock::now();
+    elapsed_msec =  end - start;
+    //結果出力
+    std::cout << "順次検索,[]オペレータ(2),std::vector," << elapsed_msec.count() << ",ミリ秒,lSum," << lSum << std::endl;
+
+    //順次検索:atメソッド(1)
+    lSum = 0;
+    start = std::chrono::system_clock::now();
+    for(long l = 0; l < oVec.size(); l++){
         lSum += oVec.at(l);
     }
     end = std::chrono::system_clock::now();
     elapsed_msec =  end - start;
     //結果出力
-    std::cout << "順次検索,atメソッド,std::vector," << elapsed_msec.count() << ",ミリ秒,lSum," << lSum << std::endl;
+    std::cout << "順次検索,atメソッド(1),std::vector," << elapsed_msec.count() << ",ミリ秒,lSum," << lSum << std::endl;
+
+    //順次検索:atメソッド(2)
+    lSum = 0;
+    start = std::chrono::system_clock::now();
+    long atMaxSize = oVec.size();
+    for(long l = 0; l < atMaxSize; l++){
+        lSum += oVec.at(l);
+    }
+    end = std::chrono::system_clock::now();
+    elapsed_msec =  end - start;
+    //結果出力
+    std::cout << "順次検索,atメソッド(2),std::vector," << elapsed_msec.count() << ",ミリ秒,lSum," << lSum << std::endl;
+#endif
+#if 1  //std::set
+    start = std::chrono::system_clock::now();
+    //要素登録
+    std::set<long> oSet;
+    for(long l = 0; l < MAX_CNT; l++){
+        oSet.insert(l);
+    }
+    end = std::chrono::system_clock::now();
+    elapsed_msec =  end - start;
+    //結果出力
+    std::cout << "要素登録処理時間,std::set," << elapsed_msec.count() << ",ミリ秒" << std::endl;
+
+    //順次検索:iterator(1)
+    lSum = 0;
+    start = std::chrono::system_clock::now();
+    for(std::set<long>::iterator it = oSet.begin(); it!=oSet.end(); it++){
+        lSum += (*it);
+    }
+    end = std::chrono::system_clock::now();
+    elapsed_msec =  end - start;
+    //結果出力
+    std::cout << "順次検索,iterator(1),std::set," << elapsed_msec.count() << ",ミリ秒,lSum," << lSum << std::endl;
+
+    //順次検索:iterator(2)
+    lSum = 0;
+    start = std::chrono::system_clock::now();
+    std::set<long>::iterator itSetE = oSet.end();
+    for(std::set<long>::iterator it = oSet.begin(); it!=itSetE; it++){
+        lSum += (*it);
+    }
+    end = std::chrono::system_clock::now();
+    elapsed_msec =  end - start;
+    //結果出力
+    std::cout << "順次検索,iterator(2),std::set," << elapsed_msec.count() << ",ミリ秒,lSum," << lSum << std::endl;
 #endif
 }
